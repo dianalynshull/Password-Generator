cc2d072738bcf55b712fff6aa1c19e170fca0021
dianalynshull <dianalynshull@gmail.com>
2020-08-01 14:44:39 -0600

    created psuedo code and added comments and console logs to the js script file

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -1,12 +1,22 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
+// define the function that is called when we press generateBtn
+
+function generatePassword() {
+  // code to run
+
+  console.log("function generatePassword is running")
+}
+
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
+
+  console.log("function writePassword is working")
 
 }
 
