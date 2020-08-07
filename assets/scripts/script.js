f0961f6c3323dd06405b135c1970d4e08d8b09df
dianalynshull <dianalynshull@gmail.com>
2020-08-07 17:38:21 -0600

    adjusted some psuedo code and js file

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -4,9 +4,13 @@ var generateBtn = document.querySelector("#generate");
 // define the function that is called when we press generateBtn
 
 function generatePassword() {
+  
   // code to run
 
-  console.log("function generatePassword is running")
+  console.log("function generatePassword is running");
+
+  // generates numbers, letters, and symbols
+
 }
 
 // Write password to the #password input
@@ -16,9 +20,14 @@ function writePassword() {
 
   passwordText.value = password;
 
-  console.log("function writePassword is working")
+  console.log("function writePassword is working");
 
 }
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
+
+
+// CONSOLE LOG EVERY FUNCTION AND VARIABLE DURING CODING TO SEE WHAT YOUR CODE IS DOING AND BEING DEFINED AS
+// MAKE SURE TO REMOVE BEFORE POSTING
---
+++ psuedo.md
@@ -0,0 +1,22 @@
+generate password
+
+To Start: user clicks on the "generate password" button
+
+collect user input for password criteria
+* length
+    * minimum 8 characters
+    * maximum 128 characters
+* symbols
+    * what type of symbols should I include?
+* numbers
+* uppercase letters
+* lowercase letters
+
+Randomize user input
+* select random characters in each categories
+    * random numbers
+    * random letters (uppercase and lowercase)
+    * random symbols
+* match the number of characters the user specified
+
+display password on the screen
