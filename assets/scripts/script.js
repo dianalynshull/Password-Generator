7136f892a0f4c87074c13b017746246256362132
dianalynshull <dianalynshull@gmail.com>
2020-08-07 18:27:52 -0600

    created function to generate password conditions. Required user to input a number between 8-128, updates various password variables to match the password generated options the user selects

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -1,9 +1,45 @@
 // Assignment Code
-var generateBtn = document.querySelector("#generate");
+let generateBtn = document.querySelector("#generate");
+let upperCase = false;
+let lowerCase = false;
+let numbers = false;
+let specialChar = false;
+
+function generateConditions() {
+  let charLimit = prompt("Select password length between 8 and 128 characters");
+  console.log(charLimit);
+
+  if (charLimit < 8 || charLimit > 128) {
+    generateConditions();
+  } else {
+    let optionLowerCase = confirm("Do you want lowercase letters in your password?");
+    lowerCase = optionLowerCase;
+    console.log(lowerCase);
+  
+    let optionUpperCase = confirm("Do you want upperCase letters in your password?");
+    upperCase = optionUpperCase;
+    console.log(upperCase);
+  
+    let optionNumbers = confirm("Do you want numbers in your password?");
+    numbers = optionNumbers;
+    console.log(numbers);
+  
+    let optionSpecialChar = confirm("Do you want special characters in your password?");
+    specialChar = optionSpecialChar;
+    console.log(specialChar);
+  };  
+};
+
+console.log(numbers);
+
+
+generateConditions();
 
 // define the function that is called when we press generateBtn
 
 function generatePassword() {
+
+  
   
   // code to run
 
@@ -15,8 +51,8 @@ function generatePassword() {
 
 // Write password to the #password input
 function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
+  let password = generatePassword();
+  let passwordText = document.querySelector("#password");
 
   passwordText.value = password;
 
