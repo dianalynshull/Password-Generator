238a1b1358a2a405ec661072bbfaafd4fffc7b2c
dianalynshull <dianalynshull@gmail.com>
2020-08-08 11:11:10 -0600

    removed characterCriteria from the generateConditions function

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -1,69 +1,59 @@
 // Assignment Code
 let generateBtn = document.querySelector("#generate");
-let upperCase = false;
-let lowerCase = false;
-let numbers = false;
-let specialChar = false;
+const passwordParams = [];
 
+// Generating password
+
+generateConditions();
 function generateConditions() {
   let charLimit = prompt("Select password length between 8 and 128 characters");
   console.log(charLimit);
 
   if (charLimit < 8 || charLimit > 128) {
     generateConditions();
-  } else {
-    let optionLowerCase = confirm("Do you want lowercase letters in your password?");
-    lowerCase = optionLowerCase;
-    console.log(lowerCase);
-  
-    let optionUpperCase = confirm("Do you want upperCase letters in your password?");
-    upperCase = optionUpperCase;
-    console.log(upperCase);
-  
-    let optionNumbers = confirm("Do you want numbers in your password?");
-    numbers = optionNumbers;
-    console.log(numbers);
-  
-    let optionSpecialChar = confirm("Do you want special characters in your password?");
-    specialChar = optionSpecialChar;
-    console.log(specialChar);
-  };  
+  };
 };
 
+let lowerCase = confirm("Do you want lowercase letters in your password?");
+console.log(lowerCase);
+
+let upperCase = confirm("Do you want upperCase letters in your password?");
+console.log(upperCase);
+
+let numbers = confirm("Do you want numbers in your password?");
 console.log(numbers);
 
+let specialChar = confirm("Do you want special characters in your password?");
+console.log(specialChar);  
 
-generateConditions();
-
-// define the function that is called when we press generateBtn
-
-function generatePassword() {
-
+if (lowerCase = true) {
+  passwordParams.push
+}
   
   
-  // code to run
+//   // code to run
 
-  console.log("function generatePassword is running");
+//   console.log("function generatePassword is running");
 
-  // generates numbers, letters, and symbols
+//   // generates numbers, letters, and symbols
 
-}
+// }
 
-// Write password to the #password input
-function writePassword() {
-  let password = generatePassword();
-  let passwordText = document.querySelector("#password");
+// // Write password to the #password input
+// function writePassword() {
+//   let password = generatePassword();
+//   let passwordText = document.querySelector("#password");
 
-  passwordText.value = password;
+//   passwordText.value = password;
 
-  console.log("function writePassword is working");
+//   console.log("function writePassword is working");
 
-}
+// }
 
-// Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
+// // Add event listener to generate button
+// generateBtn.addEventListener("click", writePassword);
 
 
 
-// CONSOLE LOG EVERY FUNCTION AND VARIABLE DURING CODING TO SEE WHAT YOUR CODE IS DOING AND BEING DEFINED AS
-// MAKE SURE TO REMOVE BEFORE POSTING
+// // CONSOLE LOG EVERY FUNCTION AND VARIABLE DURING CODING TO SEE WHAT YOUR CODE IS DOING AND BEING DEFINED AS
+// // MAKE SURE TO REMOVE BEFORE POSTING
